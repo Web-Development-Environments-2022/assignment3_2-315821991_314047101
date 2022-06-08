54696a1d2cbd0e85d78e5958ada2768fba97c51f
Naama Baruch <t-nbaruch@microsoft.com>
2022-06-08 23:26:02 +0300

    New "return 3 random recipes" function added

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -37,8 +37,51 @@ async function getRecipeDetails(recipe_id) {
     }
 }
 
+// Extract recipes data from given recipes array 
+function extractPreviewRecipeDetails(recipes_info)
+{
+    return recipes_info.map((recipes_info) => {
+        let data = recipes_info;
+        if(recipes_info.data)
+        {
+            data = recipes_info.data;
+        }
+        const {
+            id,
+            title,
+            readyInMinutes,
+            image,
+            aggregateLikes,
+            vegan,
+            vegetarian,
+            glutenFree,
+        } = data;
+        return {
+            id: id,
+            title: title,
+            readyInMinutes: readyInMinutes,
+            image: image,
+            popularity: aggregateLikes,
+            vegan: vegan,
+            vegetarian: vegetarian,
+            glutenFree: glutenFree,
+        }
+    })
+}
 
+// return 3 random recipes, by using spoonacular API
+async function getRandomThreeRecipes() {
+    const response = await axios.get(`${api_domain}/random`, {
+        params: {
+            number: 3,
+            apiKey: process.env.spooncular_apiKey
+        }
+    });
+    recipes_arr = response.data.recipes;
+    return extractPreviewRecipeDetails([recipes_arr[0],recipes_arr[1],recipes_arr[2]]);
+}
 
+exports.getRandomThreeRecipes = getRandomThreeRecipes;
 exports.getRecipeDetails = getRecipeDetails;
 
 
